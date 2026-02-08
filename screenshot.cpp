6fc68a91c251349595a8ecc3f53a10c3ea67ebcb
MarcelineVQ <marcythevq@gmail.com>
2026-02-08 14:31:10 -0800

    Async screenshot processing with queue-based worker thread
    Screenshots no longer block the game's main thread. Pixel data is
    copied and queued, a background worker drains the queue doing the
    BGR-RGB swap and JPG/PNG encoding off-thread. Timestamp-based
    filenames are generated at write time with a collision counter.

--- screenshot.cpp
+++ screenshot.cpp
@@ -2,6 +2,11 @@
 
 #include <cstdlib>
 #include <cstring>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <queue>
+#include <new>
 
 #include "screenshot.h"
 #include "utf8_to_utf16.h"
@@ -13,62 +18,138 @@ CTGAFILE_WRITE_0x5a4810 p_original_CTgaFile_Write_0x5a4810 = NULL;
 
 SCREENSHOT_FILETYPE screenshot_filetype = SCREENSHOT_FILETYPE::jpg;
 
+struct PendingScreenshot {
+    uint8_t* buffer;
+    uint16_t width;
+    uint16_t height;
+    SCREENSHOT_FILETYPE filetype;
+};
+
+static std::atomic<int> screenshots_pending{0};
+static std::mutex screenshot_mutex;
+static std::queue<PendingScreenshot> screenshot_queue;
+static bool worker_running = false;
+static char screenshot_dir[260] = {0};
+static DWORD last_write_second = 0;
+static int same_second_count = 0;
+
+static void screenshot_worker() {
+    const int bpp = 3;
+    const int quality = 95;
+
+    while (true) {
+        PendingScreenshot shot;
+
+        {
+            std::lock_guard<std::mutex> lock(screenshot_mutex);
+            if (screenshot_queue.empty()) {
+                worker_running = false;
+                screenshots_pending.fetch_sub(1);
+                return;
+            }
+            shot = screenshot_queue.front();
+            screenshot_queue.pop();
+        }
+
+        SYSTEMTIME st;
+        GetLocalTime(&st);
+        DWORD current_second = st.wDay * 86400 + st.wHour * 3600 + st.wMinute * 60 + st.wSecond;
+
+        if (current_second == last_write_second) {
+            same_second_count++;
+        }
+        else {
+            last_write_second = current_second;
+            same_second_count = 0;
+        }
+
+        char filename[260];
+        const char* ext = (shot.filetype == SCREENSHOT_FILETYPE::png) ? ".png" : ".jpg";
+        sprintf_s(filename, sizeof(filename),
+                  "%sWoWScrnShot_%02d%02d%02d_%02d%02d%02d_%d%s",
+                  screenshot_dir,
+                  st.wMonth, st.wDay, st.wYear % 100,
+                  st.wHour, st.wMinute, st.wSecond,
+                  same_second_count, ext);
+
+        // Swap BGR to RGB on our copy
+        for (int y = 0; y < shot.height; ++y) {
+            for (int x = 0; x < shot.width; ++x) {
+                uint8_t* a = shot.buffer + (x + y * shot.width) * bpp;
+                uint8_t* b = shot.buffer + (x + y * shot.width) * bpp + 2;
+
+                uint8_t temp = *a;
+                *a = *b;
+                *b = temp;
+            }
+        }
+
+        if (shot.filetype == SCREENSHOT_FILETYPE::png) {
+            stbi_write_png(filename, shot.width, shot.height, bpp, shot.buffer, shot.width * bpp);
+        }
+        else {
+            stbi_write_jpg(filename, shot.width, shot.height, bpp, shot.buffer, quality);
+        }
+
+        delete[] shot.buffer;
+    }
+}
+
 int __fastcall detoured_CTgaFile_Write_0x5a4810(uint32_t self, void* ignored, char* TGAfilename) {
-    char filename[260];
     uint8_t additionalHeaderLength = *reinterpret_cast<uint8_t*>(self + 0x8);
     uint8_t colorMapType = *reinterpret_cast<uint8_t*>(self + 0x9);
     uint8_t imageType = *reinterpret_cast<uint8_t*>(self + 0xa);
-    uint32_t tgaByteSize = *reinterpret_cast<uint32_t*>(self + 0x3c);
     uint32_t tgaHeaderAddr = self + 0x8;
     uint32_t tgaDataAddr = *reinterpret_cast<uint32_t*>(self + 0x4);
-    uint32_t tgaFooterAddr = self + 0x20;
 
     if (tgaDataAddr == NULL || TGAfilename == NULL || imageType != 2 || additionalHeaderLength != 0 || colorMapType != 0) {
         return p_original_CTgaFile_Write_0x5a4810(self, TGAfilename);
     }
 
-    if (screenshot_filetype == SCREENSHOT_FILETYPE::png) {
-        if (sprintf_s(filename, sizeof(filename), "%s%s", TGAfilename, ".png") <= 0) {
-            return p_original_CTgaFile_Write_0x5a4810(self, TGAfilename);
-        }
-    }
-    else {
-        if (sprintf_s(filename, sizeof(filename), "%s%s", TGAfilename, ".jpg") <= 0) {
-            return p_original_CTgaFile_Write_0x5a4810(self, TGAfilename);
-        }
+    // Extract directory from game's filename on first call
+    if (screenshot_dir[0] == '\0') {
+        strncpy(screenshot_dir, TGAfilename, sizeof(screenshot_dir) - 1);
+        char* last_sep = strrchr(screenshot_dir, '\\');
+        if (!last_sep) last_sep = strrchr(screenshot_dir, '/');
+        if (last_sep) *(last_sep + 1) = '\0';
+        else screenshot_dir[0] = '\0';
     }
 
     const int bpp = 3;
-    const int quality = 95;
     uint16_t width = *reinterpret_cast<uint16_t*>(tgaHeaderAddr + 0xc);
     uint16_t height = *reinterpret_cast<uint16_t*>(tgaHeaderAddr + 0xc + 2);
 
-    // Swap color channel to fit stb_image_write
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            uint8_t* a = reinterpret_cast<uint8_t*>(tgaDataAddr + (x + y * width) * bpp);
-            uint8_t* b = reinterpret_cast<uint8_t*>(tgaDataAddr + (x + y * width) * bpp + 2);
+    size_t dataSize = width * height * bpp;
+    uint8_t* buffer;
+    try {
+        buffer = new uint8_t[dataSize];
+    }
+    catch (const std::bad_alloc&) {
+        return p_original_CTgaFile_Write_0x5a4810(self, TGAfilename);
+    }
+    memcpy(buffer, reinterpret_cast<void*>(tgaDataAddr), dataSize);
 
-            uint8_t temp = *a;
-            *a = *b;
-            *b = temp;
+    {
+        std::lock_guard<std::mutex> lock(screenshot_mutex);
+        screenshot_queue.push({buffer, width, height, screenshot_filetype});
+
+        if (!worker_running) {
+            try {
+                worker_running = true;
+                screenshots_pending.fetch_add(1);
+                std::thread(screenshot_worker).detach();
+            }
+            catch (...) {
+                worker_running = false;
+                screenshots_pending.fetch_sub(1);
+                while (!screenshot_queue.empty()) {
+                    delete[] screenshot_queue.front().buffer;
+                    screenshot_queue.pop();
+                }
+                return p_original_CTgaFile_Write_0x5a4810(self, TGAfilename);
+            }
         }
     }
 
-    if (screenshot_filetype == SCREENSHOT_FILETYPE::png) {
-        if (stbi_write_png(filename, width, height, bpp, reinterpret_cast<void*>(tgaDataAddr), width * bpp) != 0) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }
-    else {
-        if (stbi_write_jpg(filename, width, height, bpp, reinterpret_cast<void*>(tgaDataAddr), quality) != 0) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }
+    return 1;
 }
